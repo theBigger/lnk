eafd3358faf76ebede4a31328f4daf07d5a6e3e0
liufei_it <liufei_it@126.com>
2015-07-24 15:34:01 +0800

    mos

--- mos-lnk/src/main/java/org/mos/lnk/parser/JsonPacketParser.java
+++ mos-lnk/src/main/java/org/mos/lnk/parser/JsonPacketParser.java
@@ -1,13 +1,6 @@
 package org.mos.lnk.parser;
 
-import org.mos.lnk.packet.InIQ;
-import org.mos.lnk.packet.InMessage;
-import org.mos.lnk.packet.InPacket;
-import org.mos.lnk.packet.InPresence;
-import org.mos.lnk.packet.InRegister;
-import org.mos.lnk.packet.InRevise;
 import org.mos.lnk.packet.Type;
-import org.mos.lnk.serializer.SerializerProvider;
 
 import net.sf.json.JSONObject;
 
@@ -17,28 +10,11 @@ import net.sf.json.JSONObject;
  * @version 1.0.0
  * @since 2015年6月13日 上午12:14:04
  */
-public class JsonPacketParser implements PacketParser {
+public class JsonPacketParser extends AbstractPacketParser {
 
 	@Override
-	public InPacket parse(String packet) throws Throwable {
+	protected Type parseType(String packet) throws Throwable {
 		JSONObject json = JSONObject.fromObject(packet);
-		Type type = Type.parse((byte) json.getInt("type"));
-		if (type == null) {
-			throw new IllegalStateException("Error Type of Packet " + packet);
-		}
-		switch (type) {
-		case IQ:
-			return SerializerProvider.deserialize(InIQ.class, packet);
-		case Message:
-			return SerializerProvider.deserialize(InMessage.class, packet);
-		case Presence:
-			return SerializerProvider.deserialize(InPresence.class, packet);
-		case Register:
-			return SerializerProvider.deserialize(InRegister.class, packet);
-		case Revise:
-			return SerializerProvider.deserialize(InRevise.class, packet);
-		default:
-			throw new IllegalStateException("Error Type of Packet " + packet);
-		}
+		return Type.parse((byte) json.getInt(TYPE));
 	}
 }
--- mos-lnk/src/main/java/org/mos/lnk/parser/XMLPacketParser.java
+++ mos-lnk/src/main/java/org/mos/lnk/parser/XMLPacketParser.java
@@ -4,14 +4,7 @@ import org.apache.commons.lang3.math.NumberUtils;
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
-import org.mos.lnk.packet.InIQ;
-import org.mos.lnk.packet.InMessage;
-import org.mos.lnk.packet.InPacket;
-import org.mos.lnk.packet.InPresence;
-import org.mos.lnk.packet.InRegister;
-import org.mos.lnk.packet.InRevise;
 import org.mos.lnk.packet.Type;
-import org.mos.lnk.serializer.SerializerProvider;
 
 /**
  * @author 刘飞 E-mail:liufei_it@126.com
@@ -19,30 +12,13 @@ import org.mos.lnk.serializer.SerializerProvider;
  * @version 1.0.0
  * @since 2015年7月22日 下午6:11:13
  */
-public class XMLPacketParser implements PacketParser {
-	
+public class XMLPacketParser extends AbstractPacketParser {
+
 	@Override
-	public InPacket parse(String packet) throws Throwable {
+	protected Type parseType(String packet) throws Throwable {
 		Document document = DocumentHelper.parseText(packet);
 		Element root = document.getRootElement();
-		String typeString = root.attributeValue("type");
-		Type type = Type.parse(NumberUtils.toByte(typeString));
-		if (type == null) {
-			throw new IllegalStateException("Error Type of Packet " + packet);
-		}
-		switch (type) {
-		case IQ:
-			return SerializerProvider.deserialize(InIQ.class, packet);
-		case Message:
-			return SerializerProvider.deserialize(InMessage.class, packet);
-		case Presence:
-			return SerializerProvider.deserialize(InPresence.class, packet);
-		case Register:
-			return SerializerProvider.deserialize(InRegister.class, packet);
-		case Revise:
-			return SerializerProvider.deserialize(InRevise.class, packet);
-		default:
-			throw new IllegalStateException("Error Type of Packet " + packet);
-		}
+		String typeString = root.attributeValue(TYPE);
+		return Type.parse(NumberUtils.toByte(typeString));
 	}
 }
