70acacd19da09c371b67bb316efc0dc92934d992
liufei_it <liufei_it@126.com>
2015-07-31 14:09:13 +0800

    ws

---
+++ mos-lnk/etc/chat.js
@@ -0,0 +1,16 @@
+/**
+ * 即时通讯协议封装
+ * @author 刘飞
+ */
+var LnkChat = (function() {
+	var WS_URL = "";
+	var chat = {
+		IQ : function(input) {
+			
+		},
+		Message : function(input) {
+			
+		}
+	};
+	return chat;
+})();
